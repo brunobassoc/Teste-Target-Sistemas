643b8a1956fa3d70e53e98dc0ab34a494e7c87c7
Bruno Basso <basso4k@gmail.com>
2024-09-02 16:56:46 -0300

    desafio 5

---
+++ lampada.js
@@ -0,0 +1,27 @@
+/* 
+5) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes.
+Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser.
+Seu objetivo é descobrir qual interruptor controla qual lâmpada. Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas,
+qual interruptor controla cada lâmpada?  
+
+
+1 etapa
+
+Ligo o interruptor A e mantenho ligado por alguns minutos. Isso vai aquecer a lâmpada conectada ao interrumptor A.
+Depois de alguns minutos, desligo o interruptor A.
+Então ligo interruptor B e o mantenho ligado.
+Deixo o interruptor C desligado o tempo todo.
+(Salas: esquerda, meio e direita)
+
+2 etapa
+
+Vou para a sala da esquerda após mexer nos interruptores.
+Se a lâmpada estiver desligada mas quente, sei que ela pertence ao interruptor A, se estiver ligada pertence a B e se estiver desligada e fria pertence a C.
+
+3 etapa (no caso da lamapda estiver desligada e quente)
+Como sei que o interruptor A pertence a sala da esquerda, agora matenho ligado o interruptor B e o C desligado.
+Vou para a sala do meio, semestiver ligado, ele pertence a sala do meio, se não, pertence a sala da direita.
+
+Problema solucionado.
+
+*/
